7426ac4e4a0fe04a3afa865b277b96ae803f92af
Son-Gyeongi <thsruddl77@gmail.com>
2023-12-22 22:42:12 +0900

    refactor : 게시글 리스트 테이블 수정

--- src/components/community/PostMain.js
+++ src/components/community/PostMain.js
@@ -1,44 +1,70 @@
 'use client';
 
 import usePosts from '@/hooks/usePosts';
+import Link from 'next/link'; // Next.js의 Link로 변경
 
 export default function PostMain() {
   const { posts, isLoading, isError, error } = usePosts();
 
   if (isLoading) {
-    return <div>로딩 중</div>;
+    return <div className="text-center mt-8">로딩 중</div>;
   }
 
   if (isError) {
-    return <div>{error}</div>;
+    return <div className="text-center mt-8">{error}</div>;
   }
 
+  const formatCreatedAt = (createdAt) => {
+    // "T" 이후의 부분을 제거하고 반환
+    return createdAt.split('T')[0];
+  };
+
   return (
-    <div className="container mx-auto">
-      <h1 className="text-4xl font-bold mb-6">게시판 리스트</h1>
+    <div className="overflow-x-auto">
+      <h3 className="text-4xl font-bold mt-6 mb-6">게시글 리스트</h3>
       {posts.listData && posts.listData.length > 0 ? (
-        <table className="min-w-full">
-          <thead>
-            <tr>
-              <th className="text-left">제목</th>
-              <th className="text-left">내용</th>
-              <th className="text-left">작성자</th>
-              <th className="text-left">작성일시</th>
-            </tr>
-          </thead>
-          <tbody>
-            {posts.listData.map((post) => (
-              <tr key={post.id} className="mb-4">
-                <td className="text-xl font-bold">{post.title}</td>
-                <td>{post.content}</td>
-                <td>{post.user.nickname}</td>
-                <td>{post.createdAt}</td>
+        <div className="overflow-x-auto">
+          <table className="table">
+            <thead className="bg-gray-200">
+              <tr>
+                <th style={{ width: '10%' }} className="py-2 px-4 border-b text-center">
+                  번호
+                </th>
+                <th style={{ width: '40%' }} className="py-2 px-4 border-b text-center">
+                  제목
+                </th>
+                <th style={{ width: '20%' }} className="py-2 px-4 border-b text-center">
+                  작성자
+                </th>
+                <th style={{ width: '30%' }} className="py-2 px-4 border-b text-center">
+                  작성일시
+                </th>
               </tr>
-            ))}
-          </tbody>
-        </table>
+            </thead>
+            <tbody>
+              {posts.listData.map((post, index) => (
+                <tr key={post.id} className="hover:bg-gray-100 transition duration-300">
+                  <td style={{ width: '10%' }} className="py-2 px-4 border-b text-center">
+                    {index + 1}
+                  </td>
+                  <td style={{ width: '40%' }} className="py-2 px-4 border-b">
+                    <Link href={`/post/${post.id}`} className="text-blue-500 hover:underline">
+                      {post.title}
+                    </Link>
+                  </td>
+                  <td style={{ width: '20%' }} className="py-2 px-4 border-b text-center">
+                    {post.user.nickname}
+                  </td>
+                  <td style={{ width: '30%' }} className="py-2 px-4 border-b text-center">
+                    {formatCreatedAt(post.createdAt)}
+                  </td>
+                </tr>
+              ))}
+            </tbody>
+          </table>
+        </div>
       ) : (
-        <div>현재 게시물이 없습니다.</div>
+        <div className="text-center mt-8">현재 게시물이 없습니다.</div>
       )}
     </div>
   );
