2f66f551c889d9a927ed1238cab54074ebcb64ff
kyumho kim <js030@github.com>
2024-01-07 18:52:00 +0900

    merge : 경이님 branch 병합

--- src/hooks/usePosts.js
+++ src/hooks/usePosts.js
@@ -1,15 +1,15 @@
-import { useMutation, useQuery, useQueryClient } from '@tanstack/react-query';
-import { apiAxios } from '@/config/axios-config';
-import { getPostById } from '@/api/post';
+import { useMutation, useQuery, useQueryClient } from '@tanstack/react-query'
+import axios from '@/config/axios-config'
+import { getPostById } from '@/api/post'
 
 // axios ver
 const fetchPosts = async () => {
   const {
     data: { listData },
-  } = await apiAxios.get('/posts');
+  } = await axios.get('api/posts/list')
 
-  return listData;
-};
+  return listData
+}
 
 // 모든 게시글 조회
 export function usePosts() {
@@ -21,9 +21,9 @@ export function usePosts() {
   } = useQuery({
     queryKey: ['posts'],
     queryFn: fetchPosts,
-  });
+  })
 
-  return { posts, isLoading, isError, error };
+  return { posts, isLoading, isError, error }
 }
 
 // 게시글 ID로 조회
@@ -36,22 +36,22 @@ export function usePost(postId) {
   } = useQuery({
     queryKey: ['post', postId],
     queryFn: async () => getPostById(postId), // 수정된 부분
-  });
+  })
 
-  return { post, isLoading, isError, error };
+  return { post, isLoading, isError, error }
 }
 
 const fetchWritePost = async (formData) => {
   for (let [key, value] of formData.entries()) {
-    console.log(key, value);
+    console.log(key, value)
   }
 
-  return await apiAxios.post('/posts/write', formData);
-};
+  return await apiAxios.post('/posts/write', formData)
+}
 
 // 게시글 저장
 export const useWritePost = () => {
-  const queryClient = useQueryClient();
+  const queryClient = useQueryClient()
   const {
     mutate: submitWrite,
     isPending,
@@ -60,19 +60,19 @@ export const useWritePost = () => {
   } = useMutation({
     mutationFn: (formData) => {
       for (let [key, value] of formData.entries()) {
-        console.log(key, value);
+        console.log(key, value)
       }
-      return fetchWritePost(formData);
+      return fetchWritePost(formData)
     },
     onSuccess: (res) => {
-      console.log(res);
+      console.log(res)
 
-      queryClient.invalidateQueries({ queryKey: ['posts'] });
+      queryClient.invalidateQueries({ queryKey: ['posts'] })
     },
     onError: (err) => {
-      console.log(err);
+      console.log(err)
     },
-  });
+  })
 
-  return { submitWrite, isPending, isError, error };
-};
+  return { submitWrite, isPending, isError, error }
+}
