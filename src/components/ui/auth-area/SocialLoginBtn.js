761a16846b4f3b675e71d3d7f5f18bf4b6ff278b
taehoon <xogns2134@gmail.com>
2024-01-08 17:49:20 +0900

    social button

--- src/components/ui/auth-area/SocialLoginBtn.js
+++ src/components/ui/auth-area/SocialLoginBtn.js
@@ -1,12 +1,17 @@
 /** @format */
 
 import { authSocialBtnType } from '@/constants/auth'
+import axios from '../../../config/axios-config'
 
 export default function SocialLoginBtn({ socialType }) {
   const type = authSocialBtnType[socialType]
+  const handleOAuthLogin = async() => {
+    const response = await axios.get(`/api/oauth/socialLogin/${socialType}`)
+    console.log(response)
+  }
 
   return (
-    <button
+    <button onClick={handleOAuthLogin}
       className={`flex items-center justify-center pt-4 pr-5 pb-4 pl-5 text-center w-full text-black p-3 duration-300 rounded-lg ${type?.color}`}>
       {type.Icon()}
       <span className='ml-2'>{type.title}</span>
