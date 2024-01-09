d69693b7246a459ebf8bbb180e849edc9bb8fd36
kyumho kim <js030@github.com>
2024-01-09 18:04:47 +0900

    try : 배포 시도5

--- src/app/api/posts/write/route.js
+++ src/app/api/posts/write/route.js
@@ -1,13 +1,12 @@
-import { savePost } from '@/api/post';
-import { NextResponse } from 'next/server';
+import { NextResponse } from 'next/server'
 
 export async function POST(req) {
-  const formData = await req.formData();
+  const formData = await req.formData()
   for (let [key, value] of formData.entries()) {
-    console.log(key, value);
+    console.log(key, value)
   }
 
   return savePost(formData)
     .then((res) => NextResponse.json(res))
-    .catch((error) => new Response(JSON.stringify(error), { status: 500 }));
+    .catch((error) => new Response(JSON.stringify(error), { status: 500 }))
 }
