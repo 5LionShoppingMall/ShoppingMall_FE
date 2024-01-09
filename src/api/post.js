2a017c6411b83ad31654cd8604f55a25de931569
kyumho kim <js030@github.com>
2024-01-09 18:13:31 +0900

    try : 배포 시도7

--- src/api/post.js
+++ src/api/post.js
@@ -1,5 +1,5 @@
-const BASE_URL = 'http://localhost:8082';
-const PATH_POST = '/api/posts';
+const BASE_URL = 'https://api.lionshop.me'
+const PATH_POST = '/api/posts'
 
 // 모든 게시글 조회
 export const getPosts = async () => {
@@ -8,21 +8,21 @@ export const getPosts = async () => {
       'Content-Type': 'application/json',
     },
     cache: 'no-store',
-  });
+  })
 
-  const data = await res.json();
+  const data = await res.json()
 
-  return data;
-};
+  return data
+}
 
 // 게시글 ID로 조회
 export const getPostById = async (postId) => {
   try {
-    const res = await fetch(`${BASE_URL}${PATH_POST}/detail/${postId.postId}`);
-    const data = await res.json();
-    return data;
+    const res = await fetch(`${BASE_URL}${PATH_POST}/detail/${postId.postId}`)
+    const data = await res.json()
+    return data
   } catch (error) {
-    console.log(error);
-    return error;
+    console.log(error)
+    return error
   }
-};
+}
