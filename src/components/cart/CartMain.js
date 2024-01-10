f7ecdc6167691498d1b83e16be869f7518283b4d
daseul-jang <devzzang94@gmail.com>
2024-01-10 10:12:35 +0900

    fix: 오류 수정

--- src/components/cart/CartMain.js
+++ src/components/cart/CartMain.js
@@ -4,10 +4,9 @@ import { useUser } from '@/hooks/useUser';
 
 export default function CartMain() {
   const { user } = useUser();
-  console.log(user);
   return (
     <div>
-      <div>{user.email}</div>
+      <div>{user?.email}</div>
     </div>
   );
 }
