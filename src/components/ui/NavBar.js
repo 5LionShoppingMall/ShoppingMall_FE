8420eb51c5c25c2550f3258507a2b805d2c6294e
kyumho kim <js030@github.com>
2024-01-31 21:16:16 +0900

    test : 배포 되는지 테스트

--- src/components/ui/NavBar.js
+++ src/components/ui/NavBar.js
@@ -1,95 +1,85 @@
 'use client'
 
 import Link from 'next/link'
+import CategoryMenu from './ui/navbar-menu/CategoryMenu'
+import HotelIcon from './ui/icon/HotelIcon'
+import Dropdown from './ui/navbar-menu/Dropdown'
 import { useUser } from '@/hooks/useUser'
-import { useState } from 'react'
-
-import Dropdown from './navbar-menu/Dropdown'
-import CategoryMenu from './navbar-menu/CategoryMenu'
-import NavbarIconBtn from './navbar-menu/NavbarIconBtn'
-import SearchIcon from './icon/SearchIcon'
-import SaleIcon from './icon/SaleIcon'
-import { useRouter } from 'next/navigation'
+import axios from '@/config/axios-config'
 
 export default function Navbar() {
   const { user, isLoading, isError } = useUser()
-  const [keyword, setKeyword] = useState('')
 
-  const router = useRouter()
-  const handleSearch = () => {
-    router.push(`/search?kw=${keyword}`)
-  }
+  const handleLogout = async (e) => {
+    e.preventDefault()
 
-  const handleKeyDown = (e) => {
-    if (e.key === 'Enter' && e.target.value !== '') {
-      handleSearch()
-    }
+    const response = await axios
+      .delete('/user/logout', { ...axios.defaults, useAuth: true })
+      .then((res) => {
+        console.log(res)
+        sessionStorage.clear()
+        window.location.href = '/'
+      })
+      .catch((err) => {
+        console.log(err)
+      })
   }
 
   return (
-    <div className='navbar bg-transparent'>
-      <div className='flex-1 gap-2'>
+    <div className='navbar flex bg-transparent'>
+      <div className='flex flex-1 gap-2'>
         <Link
           href='/'
-          className='font-semibold text-2xl flex justify-center items-center px-3 font-jeju text-sage-750 dark:text-coral-500'>
-          LION
+          className='font-semibold text-2xl flex justify-center items-center px-3 text-sage-750 dark:text-coral-500'>
+          HOTSIX
         </Link>
         <div className='hidden lg:flex'>
-          <ul className='flex gap-2 px-1 text-sage-700 dark:text-cream text-sm font-semibold tracking-widest'>
+          <ul className='flex items-center gap-2 px-1 text-sage-700 dark:text-cream text-sm font-semibold tracking-widest'>
             <CategoryMenu />
           </ul>
         </div>
       </div>
       <div className='flex-none gap-1'>
-        <div className='lg:hidden flex gap-1'>
-          <NavbarIconBtn menu='search' />
-          {user && <NavbarIconBtn menu='write' />}
-        </div>
+        <div className='lg:hidden flex gap-1'></div>
         <div className='hidden lg:flex flex-none gap-10 h-full items-center'>
-          <div className='relative bg-zinc-100 rounded-full flex items-center h-10'>
-            <div className='absolute flex items-center inset-y-0 left-0 pl-3 pointer-events-none'>
-              <button onClick={handleSearch}>
-                <SearchIcon />
-              </button>
-            </div>
-            <input
-              type='text'
-              placeholder='Search'
-              className='bg-transparent pl-10 pr-5 focus:outline-none text-sm text-neutral-800'
-              value={keyword}
-              onChange={(e) => setKeyword(e.target.value)}
-              onKeyDown={handleKeyDown}
-            />
+          <div className='flex gap-4'>
+            {user ? (
+              <div className='flex space-x-4'>
+                <p className='text-sm flex items-center mr-3'>
+                  {user?.objData.nickname}님 환영합니다
+                </p>
+                <Link
+                  href='/hotel/register'
+                  className='text-sm flex items-center justify-center gap-1'>
+                  <HotelIcon />
+                  숙소 등록
+                </Link>
+                <Link href='/auth/logout'>
+                  <button
+                    onClick={handleLogout}
+                    className='bg-sage-600 text-sm font-semibold py-3 px-4'>
+                    로그아웃
+                  </button>
+                </Link>
+              </div>
+            ) : (
+              <>
+                <Link href='/auth/signin'>
+                  <button className='bg-sage-600 text-sm font-semibold py-3 px-4'>
+                    로그인
+                  </button>
+                </Link>
+                <Link href='/auth/signup'>
+                  <button className='bg-sage-600 text-sm font-semibold py-3 px-4'>
+                    회원가입
+                  </button>
+                </Link>
+              </>
+            )}
           </div>
-          {user ? (
-            <div className='flex gap-4'>
-              <Link
-                href='/products/write'
-                className='text-sm flex items-center justify-center gap-1'>
-                <SaleIcon />
-                거래등록
-              </Link>
-              <NavbarIconBtn menu='cart' />
-              <Dropdown menu='avatar' />
-              <p className='mt-3'>{user?.nickname} 님 환영합니다.</p>
-            </div>
-          ) : (
-            <div className='flex justify-center items-center gap-2'>
-              <Link href='/auth/signin'>
-                <button className='bg-sage-600 text-sm font-semibold text-white rounded-full py-3 px-4 tracking-widest hover:bg-sage-650 dark:bg-transparent dark:text-cream dark:hover:bg-transparent dark:hover:text-coral-400'>
-                  로그인
-                </button>
-              </Link>
-              <Link href='/auth/signup'>
-                <button className='bg-sage-600 text-sm font-semibold text-white rounded-full py-3 px-4 tracking-widest hover:bg-sage-650 dark:bg-transparent dark:text-cream dark:hover:bg-transparent dark:hover:text-coral-400'>
-                  회원가입
-                </button>
-              </Link>
-            </div>
-          )}
         </div>
-        <Dropdown menu='dropdown' />
       </div>
+      <Dropdown menu='dropdown' />
     </div>
   )
 }
